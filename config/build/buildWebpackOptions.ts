f46bdbd9719e4f6b193bfda0c61e80c6549e1440
Daniil Akhiyan <daniil.axiyan.316@mail.ru>
2024-04-15 23:53:30 +0300

    initial commit

---
+++ config/build/buildWebpackOptions.ts
@@ -0,0 +1,28 @@
+import path from "path";
+import { BuildOptions } from "./types/config";
+import webpack from "webpack";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildPlugins } from "./buildPlugins";
+import { buildDevServer } from "./buildDevServer";
+
+export function buildWebpackOptions(options: BuildOptions): webpack.Configuration {
+    const { mode, paths } = options;
+
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true,
+        },
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(),
+        plugins: buildPlugins(options),
+        devtool: "inline-source-map",
+        devServer: buildDevServer(options),
+    }
+}
---
+++ webpack.config.ts
@@ -0,0 +1,30 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { buildPlugins } from './config/build/buildPlugins';
+import { buildLoaders } from './config/build/buildLoaders';
+import { buildResolvers } from './config/build/buildResolvers';
+import { buildWebpackOptions } from './config/build/buildWebpackOptions';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
+
+
+
+export default (env : BuildEnv) => {
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    }
+    
+    const mode = env.mode || 'development';
+    const isDev = mode === 'development';
+    const PORT = env.port || 3000;
+    
+    const config:webpack.Configuration = buildWebpackOptions({
+        mode,
+        paths,
+        isDev,
+        port: PORT
+    });
+    return config;
+};
